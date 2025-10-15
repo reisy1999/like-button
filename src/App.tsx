bd5de44b58aa88c2ad399f04cdc61c67e78631b4
reisy1999 <abereneibu@gmail.com>
2025-10-16 05:38:17 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import "./App.css";
+import { useState } from "react"; //reactってどこからひっぱってきてるんだ?
 
 function App() {
   return (
     <>
-      <LikeButton/> // セルフクロージング要素
+      <LikeButton/>
     </>
   );
 }
 
 function LikeButton() {
-  return <span>ここにいいね</span>;
+  const [count, setCount] = useState(100);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className= "likeButton" onClick ={handleClick}>
+      ♡ {count}
+    </span>
+  ); 
 }
 
 export default App;
